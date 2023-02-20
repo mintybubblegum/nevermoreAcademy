e04135f49e388a0fd0af605ed6596b2c64e5b5ef
AlesiaCoder <alesia.nekolethal@gmail.com>
2023-02-20 21:07:34 +0100

    resolving conflicts

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,16 +19,17 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'surname' => fake()->name(),
+            'surname' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '', // password
             'remember_token' => Str::random(10),
             'currentTerm' => fake()->name(),
             'img' => fake()->image(),
             'isTeacher' => false,
         ];
     }
+
 
     /**
      * Indicate that the model's email address should be unverified.
